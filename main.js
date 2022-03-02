431eead412c6fa66ecf79b184b3b9fda1763bd83
Aakash Yadav <yaakash59.ay@gmail.com>
2022-03-02 21:47:00 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const add=require("./add");
+const subtract=require("./subtract");
+const multiply=require("./multiply");
+const divide=require("./divide");
+
+console.log("addition:",add(2,4));
+console.log("subtraction:",subtract(5,2));
+console.log("multiplication:",multiply(2,4));
+console.log("division:",divide(10,2));
